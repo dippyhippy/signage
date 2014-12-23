a9d7067e9cc55441cd9f44ac295da0ca6142a93a
dippyhippy <ben@dippyhippy.com>
2014-12-23 22:19:49 +0000

    stuff

--- components/Create_Graph.js
+++ components/Create_Graph.js
@@ -1,19 +1,42 @@
 var noflo = require('noflo');
 
 exports.getComponent = function () {
-  var c = new noflo.Component();
+  var c = new noflo.Component(),
+      g = new noflo.Graph();
 
   c.inPorts.add('name', function (event, payload) {
-    c.outPorts.log.send('event: ' + event);
+    
     if (event !== 'data') {
       return;
     }
+    
+    c.outPorts.log.send('event: ' + event + ', payload: ' + JSON.stringify(payload));
+    c.outPorts.log.send('typeof payload: ' + typeof(payload));
+    
     // Do something with the packet, then
-    if (typeof(data)==='string') {
-      c.outPorts.out.send(new Graph(data));
+    if (typeof(payload)==='string') {
+      g.name = payload;
+      c.outPorts.graph.send(g);
     }
   });
+  
+  c.inPorts.add('save', function (event, payload) {
+    
+    if (event !== 'data') {
+      return;
+    }
+    
+    c.outPorts.log.send('event: ' + event + ', payload: ' + JSON.stringify(payload));
+    c.outPorts.log.send('typeof payload: ' + typeof(payload));
+    
+    // Do something with the packet, then
+    if (typeof(payload)==='string') {
+      c.outPorts.json.send(g.toJSON);
+    }
+  });
+  
   c.outPorts.add('graph');
   c.outPorts.add('log');
+  c.outPorts.add('json');
   return c;
 };
