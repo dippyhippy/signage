21e4e9e0f57a54e78536f432afe6d0e71389043e
Ben Major <ben@godalming.it>
2014-12-23 20:44:03 +0000

    changed some ting

--- src/loader.js
+++ src/loader.js
@@ -3,6 +3,11 @@
  */
 var loader = function(loader, done) {
     console.log('loading components');
+
+    console.log('registerComponent: %s', typeof(loader.registerComponent)==='function');
+    console.log('registerGraph: %s', typeof(loader.registerGraph)==='function');
+
+
     done();
 };
 
